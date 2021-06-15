636efc75aeb76d545a51d96f84eb340b25e9fbe7
ldh7228 <ldh7228@gmail.com>
2021-06-15 12:34:48 +0900

    create timer

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const number = document.getElementById('number');
+const timer = document.getElementById('timer');
+let num = 0;
+let time = 10;
+timer.innerHTML = `${time}s`;
 function printName () {
-    const word = document.querySelector('#word').textContent
-    const answer = document.querySelector('#answer').value
+    const word = document.querySelector('#word').textContent;
+    const answer = document.querySelector('#answer').value;
     let lastIndex = word.length - 1;
-    if (word[lastIndex] === answer[0]) {
+    number.innerHTML = `${num}번 하였습니다.`;
+    if (word[lastIndex] === answer[0] && answer.length > 1) {
         const name = document.getElementById('answer').value;
         document.getElementById("word").innerText = name;
+        num++;
     }
     else { 
         alert('틀렸습니다!');
     }
-    answer.value = '';
 }
+let timeOut = setInterval(function(){
+    if (time > 0) {
+        time--;
+        timer.innerHTML = `${time}s`;
+    }
+}, 1000)
